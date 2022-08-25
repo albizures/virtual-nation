2d23f285a9217a718a9116b1ea19df166d6ac235
Jose Albizures <albizures3601@gmail.com>
2022-08-24 19:54:59 -0600

    feat: move to top

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -36,51 +36,6 @@ export function Hero() {
 				playsInline={true}
 				autoPlay={true}
 			/>
-			<h1 className="text-2xl md:text-4xl relative mt-30 text-center font-sans uppercase font-bold text-shadow-sm">
-				<span className="block">The virtual nation app </span>
-				<span className="block">gives creators</span>
-				<span className="block underline underline-nation underline-4">
-					superpowers
-				</span>
-			</h1>
-
-			<div className="absolute w-full bottom-0 py-8 px-6">
-				<div className="flex max-w-5xl mx-auto justify- md:justify-between">
-					<div className="md:flex flex-1 items-center -ml-4 hidden ">
-						<Arrows
-							amount={3}
-							className="transform rotate-90 text-2xl -mr-3"
-						/>
-						<span className="uppercase write-orient-upright write-vertical-left text-xs">
-							scroll
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row items-center">
-						<div className="flex md:mr-12 items-center space-x-12 justify-center">
-							<Feature label="24/7 Support" img={supportImg} />
-							<Feature label="Easy to use" img={configImg} />
-							<Feature label="Private Data" img={lockImg} />
-						</div>
-						<div className="flex items-center space-x-4 md:space-x-12 mt-8 md:mt-0">
-							<div>
-								<img
-									className="grow-1 h-12"
-									src={googleImg}
-									alt="google"
-								/>
-							</div>
-							<div>
-								<img
-									className="grow-1 h-12"
-									src={appleImg}
-									alt="apple"
-								/>
-							</div>
-						</div>
-					</div>
-					<div className="hidden md:block flex-1">{/* space */}</div>
-				</div>
-			</div>
 		</ScreenSection>
 	);
 }
